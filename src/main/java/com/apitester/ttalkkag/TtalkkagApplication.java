b302b0c4df56e54973ce7c938e5c508d6f6cd161
정다영 <jdayoung1204@naver.com>
2024-11-26 18:00:14 +0900

    Initial commit

---
+++ src/main/java/com/apitester/ttalkkag/TtalkkagApplication.java
@@ -0,0 +1,13 @@
+package com.apitester.ttalkkag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtalkkagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtalkkagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apitester/ttalkkag/TtalkkagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apitester.ttalkkag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtalkkagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
